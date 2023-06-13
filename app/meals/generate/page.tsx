6c58f29ed0075230797545ed433dd2db23f360d2
Karsten Fisk <karstenfisk7@gmail.com>
2023-06-13 16:49:59 -0700

    Setup client for development

---
+++ app/meals/generate/page.tsx
@@ -0,0 +1,7 @@
+export default function Generate() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 text-2xl">
+      Meal generator
+    </main>
+  );
+}
---
+++ components/Navbar.tsx
@@ -0,0 +1,57 @@
+"use client";
+import Link from "next/link";
+import { signIn, signOut } from "next-auth/react";
+import { useSession } from "next-auth/react";
+
+export default function Navbar() {
+  const { data: session, status } = useSession();
+  return (
+    <div className="shadow-sm w-screen h-14 flex items-center justify-between">
+      <Link href="/">
+        <h5 className="text-4xl font-sans font-bold font-main pl-5 italic tracking-tighter">
+          Food
+        </h5>
+      </Link>
+
+      <div className="pr-10">
+        {status === "loading" ? null : session?.user ? (
+          <>
+            <Link
+              href="/meals/generate"
+              className="font-semibold underline-offset-2 hover:text-sky-600  px-4"
+            >
+              Generate Meal
+            </Link>
+            <button
+              onClick={() => signOut()}
+              className="underline underline-offset-2 hover:text-red-500 px-4"
+            >
+              Sign Out
+            </button>
+          </>
+        ) : (
+          <>
+            <Link
+              href="/meals/generate"
+              className="font-semibold underline-offset-2 hover:text-sky-600  px-4"
+            >
+              Generate Meal
+            </Link>
+            <button
+              onClick={() => signIn()}
+              className="underline underline-offset-2 hover:text-sky-600 px-4"
+            >
+              Login
+            </button>
+            <Link
+              href="/register"
+              className="underline underline-offset-2 hover:text-sky-600  px-4"
+            >
+              Sign Up
+            </Link>
+          </>
+        )}
+      </div>
+    </div>
+  );
+}
